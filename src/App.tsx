2e0edb28f1670a75bb1d95c4ca87cbefb2a1862d
PotterFH <ivan_2333@outlook.com>
2024-05-18 12:04:12 -0600

    Avance

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import BudgetForm from './componentes/BudgetForm';
-
-
-
-
+import { useMemo } from "react";
+import BudgetForm from "./componentes/BudgetForm";
+import { useBudget } from "./hooks/useBudget";
+import BudgetTracker from "./componentes/BudgetTracker";
+import ExpenseModal from "./componentes/ExpenseModal";
 function App() {
+  const { state } = useBudget();
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
+
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
-        <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de gastos</h1>
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de gastos
+        </h1>
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-          <BudgetForm/>
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        </main>
+      )}
     </>
   );
 }
--- src/componentes/BudgetForm.tsx
+++ src/componentes/BudgetForm.tsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { useBudget } from "../hooks/useBudget";
 
 export default function BudgetForm() {
+
   const [budget, setBudget] = useState(0);
+  const { dispatch } = useBudget();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.name);
-    console.log(e.target.id);
+  setBudget(e.target.valueAsNumber);
   };
 
+  const isValid = useMemo ( () => {
+    return isNaN(budget) || budget <= 0 
+  }, [budget]) 
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    dispatch({type: 'add-budget', payload: {budget}})
+  }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <div className="flex flex-col space-y-5">
         <label
           htmlFor="budget"
@@ -19,8 +31,8 @@ export default function BudgetForm() {
           Definir Presupuesto
         </label>
         <input
+          id="budget"
           type="number"
-          id="budgetID"
           className="w-full bg-white border border-gray-200 p-2"
           placeholder="Define tu presupuesto"
           name="budget"
@@ -30,9 +42,10 @@ export default function BudgetForm() {
       </div>
 
       <input
-        type="submnit"
+        type="submit"
         value="definir Presupuesto"
-        className="bg-blue-600 hover:bg-blue-600 cursor-pointer w-full p-2 text-white font-bold uppercase text-center"
+        className="bg-blue-600 hover:bg-blue-600 cursor-pointer w-full p-2 text-white font-bold uppercase text-center disabled:opacity-40"
+        disabled={isValid}
       />
     </form>
   );
